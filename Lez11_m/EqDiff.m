e209e4035ee2b7ad51dde539c7302549fcf6d2cd
Stefano Mandelli <algebrato@lcm.mi.infn.it>
2015-03-30 14:54:42 +0200

    modified:   EqDiff.m

--- Lez11_m/EqDiff.m
+++ Lez11_m/EqDiff.m
@@ -1,6 +1,17 @@
-(*Sol[B_,O_]=NDSolve[{f''[t] + B*x'[t] + O^2*x[t] ==0, f'[0]==0, f[0] == 1},f[t],t]*)
-
-
-f[beta_,omega_] = y[x] /. DSolve[ {y''[t] + beta * y'[t] + O^2*y[t] ==0, 
+(*Disegno le curve al variare dei parametri*)
+g[beta_,omega_] = y[x] /. DSolve[ {y''[x] + beta*y'[x] + (omega*omega)*y[x] ==0, 
 				   y'[0]==0, 
-				   f[0] == 1},   y[t],  t];
+				   y[0] == 1},   y[x],  x];
+gra1= Plot [g[0,1] , {x,0, 2 Pi}];
+gra2= Plot [g[0.5,1] , {x,0, 2 Pi}];
+gra3= Plot [g[3,1] , {x,0, 2 Pi}];
+
+
+g[omega_] = y[x] /. Dsolve[ {y''[x] + 2 omega y'[x] + omega^2 y[x] ==0,
+                                   y'[0] ==0,
+                                   y[0]  == 1 },   y[x],  x];
+
+f[beta_, omega_] := If[ beta === 2 omega, g[omega], g[beta,omega]] (*Non funzione con l'uguale, il := se lo salva ma non lo valuta, lo valuterà solo successivamente*)
+
+
+
