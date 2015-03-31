a49899457b13f5977cbcec3f4f988471fa833af0
Stefano Mandelli <algebrato@lcm.mi.infn.it>
2015-03-31 21:51:52 +0200

    modified:   Lez12_m/C++/list.cpp

--- Lez12_m/C++/list.cpp
+++ Lez12_m/C++/list.cpp
@@ -13,10 +13,11 @@ int main(){
 	list<int> a = {1,2,3,4,5};
 	list<int> b = {22,33,44,55,66,77} ;
 	
-	list<int>:: iterator it= a.begin()++++++;
-	a.splice(it, b, b.begin()++++++, b.end() );
+	list<int>::iterator it= ++++a.begin();
+	a.splice(it, b, b.begin(), b.end() );
 
-	for(it = a.begin(); it!= a.end(); ++it) cout << *it << endl;
+	list<int>::iterator it2;
+	for(it2 = a.begin(); it2!= a.end(); ++it2) cout << *it2 << endl;
 	
 }
 
