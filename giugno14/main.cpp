41296c7f4b18f4e436518141a69e1d5ec87a9a1c
Stefano Mandelli <algebrato@lcm.mi.infn.it>
2015-04-18 17:21:23 +0200

    Non torna nulla T_T
    	modified:   giugno14/main.cpp

--- giugno14/main.cpp
+++ giugno14/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 class Sequence{
     std::vector<int> v_ = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-    std::vector<int> seq_;
     int a_,b_,c_;
-
+    std::vector<int> seq_;
 public:
+
     //CTOR
     Sequence(int a=0, int b=0, int c=0) : a_(a), b_(b), c_(c){
         for(int i=0; i<16; ++i){
@@ -20,13 +20,13 @@ public:
     };
 
     //CopyCTOR
-    Sequence(const Sequence &c) : a_(c.a_), b_(c.b_), c_(c.c_), seq_(c.seq_){};
+    explicit Sequence(const Sequence &c) : a_(c.a_), b_(c.b_), c_(c.c_), seq_(c.seq_){};
 
     virtual void print(){
-        cout << "Print della sequenza:" << endl;
+        cout << "Print della sequenza generale:" << endl;
         for(auto & elem : seq_)
             cout << elem << endl;
-    }
+    };
 
     int operator*(Sequence &destra) const {
         int prod=0;
@@ -37,21 +37,50 @@ public:
     }
 };
 
-/*class BinarySeq : public Sequence {
+class BinarySeq : public Sequence {
+    std::vector<int> seqb_;
 public:
-    BinarySeq(const Sequence &s) : Sequence(s) {
-        for(int i=0; i<16;i++){
-            if(rand()<0.5){
-                s.seq_.push_back(0);
-            else
-                s.seq_.push_back(1);
-            }
 
-        }
+    BinarySeq(int a=0, int b=0, int c=0, int d=0){
+
+       for(int i=0;i<16;i++){
+           double a = (double) rand() / RAND_MAX ;
+           if(a<0.5) seqb_.push_back(0);
+           else seqb_.push_back(1);
+       }
+   };
+    explicit BinarySeq(const BinarySeq &bs) : seqb_(bs.seqb_){};
+    BinarySeq(std::vector<int> seqnew) : seqb_(seqnew){};
+    void print(){
+       cout << "Print della sequenza binaria:" << endl;
+       for(auto & elem : seqb_)
+           cout << elem << endl;
     }
+    std::vector<int> get_bin_vector(){
+       return seqb_;
+   }
+};
 
+
+
+
+/*BinarySeq Not(const BinarySeq &bs) {
+    std::vector<int> vettorenot;
+    std::vector<int> seqiniz(bs.get_bin_vector());
+    for(std::vector<int>::iterator it=seqiniz.begin() ; it != seqiniz.end(); ++it){
+        if(*it = 1){
+            vettorenot.push_back(0);
+        }else{
+            vettorenot.push_back(1);
+        }
+
+    }
+    return BinarySeq(vettorenot);
 };
 */
+
+
+
 
 
 int main()
@@ -70,13 +99,12 @@ int main()
     cout << "stampa di B*A" << endl;
     cout << d << endl;
 
-/*    Sequence *bin;
+    Sequence *bin;
     bin=new BinarySeq;
-
+    a.print();
     bin->print();
-*/
+
+
 
     return 0;
 }
-
-
